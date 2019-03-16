b80255ce60d54a442934b827cd564e8c72bcec9b
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 00:31:36 +0530

    Update pages.js

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -38,7 +38,7 @@ class PagesRoute {
 	getPages(req, res, next) {
 		PagesService.getPages(req.query)
 			.then(data => {
-				res.send(data);
+				return res.send(data);
 			})
 			.catch(next);
 	}
@@ -47,9 +47,9 @@ class PagesRoute {
 		PagesService.getSinglePage(req.params.id)
 			.then(data => {
 				if (data) {
-					res.send(data);
+					return res.send(data);
 				} else {
-					res.status(404).end();
+					return res.status(404).end();
 				}
 			})
 			.catch(next);
@@ -58,7 +58,7 @@ class PagesRoute {
 	addPage(req, res, next) {
 		PagesService.addPage(req.body)
 			.then(data => {
-				res.send(data);
+				return res.send(data);
 			})
 			.catch(next);
 	}
@@ -67,9 +67,9 @@ class PagesRoute {
 		PagesService.updatePage(req.params.id, req.body)
 			.then(data => {
 				if (data) {
-					res.send(data);
+					return res.send(data);
 				} else {
-					res.status(404).end();
+					return res.status(404).end();
 				}
 			})
 			.catch(next);
@@ -78,7 +78,7 @@ class PagesRoute {
 	deletePage(req, res, next) {
 		PagesService.deletePage(req.params.id)
 			.then(data => {
-				res.status(data ? 200 : 404).end();
+				return res.status(data ? 200 : 404).end();
 			})
 			.catch(next);
 	}
