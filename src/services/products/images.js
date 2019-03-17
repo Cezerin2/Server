06cb4da5bfe4901dff20825c62ac23e90f97b52a
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 09:21:53 +0530

    Update images.js

--- src/services/products/images.js
+++ src/services/products/images.js
@@ -10,8 +10,6 @@ import parse from '../../lib/parse';
 import SettingsService from '../settings/settings';
 
 class ProductImagesService {
-	constructor() {}
-
 	getErrorMessage(err) {
 		return { error: true, message: err.toString() };
 	}
