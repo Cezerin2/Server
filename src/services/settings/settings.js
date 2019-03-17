c9948443cc048daa9d456a1c49e55b66bb15934f
Nitin Goyal <nitingoyal.dev@gmail.com>
2019-03-17 10:23:37 +0530

    Update settings.js

--- src/services/settings/settings.js
+++ src/services/settings/settings.js
@@ -81,80 +81,71 @@ class SettingsService {
 
 		let settings = {};
 
-		if (data.store_name !== undefined) {
+		if (data.store_name) {
 			settings.store_name = parse.getString(data.store_name);
 		}
 
-		if (data.language !== undefined) {
+		if (data.language) {
 			settings.language = parse.getString(data.language);
 		}
 
-		if (data.currency_code !== undefined) {
+		if (data.currency_code) {
 			settings.currency_code = parse.getString(data.currency_code);
 		}
 
-		if (data.domain !== undefined) {
+		if (data.domain) {
 			settings.domain = parse.getString(data.domain);
 		}
 
-		if (data.currency_symbol !== undefined) {
+		if (data.currency_symbol) {
 			settings.currency_symbol = parse.getString(data.currency_symbol);
 		}
 
-		if (data.currency_format !== undefined) {
+		if (data.currency_format) {
 			settings.currency_format = parse.getString(data.currency_format);
 		}
 
-		if (data.thousand_separator !== undefined) {
+		if (data.thousand_separator) {
 			settings.thousand_separator = parse.getString(data.thousand_separator);
 		}
 
-		if (data.decimal_separator !== undefined) {
+		if (data.decimal_separator) {
 			settings.decimal_separator = parse.getString(data.decimal_separator);
 		}
 
 		if (data.decimal_number !== undefined) {
-			settings.decimal_number =
-				parse.getNumberIfPositive(data.decimal_number) || 0;
+			settings.decimal_number = parse.getNumberIfPositive(data.decimal_number) || 0;
 		}
 
-		if (data.timezone !== undefined) {
+		if (data.timezone) {
 			settings.timezone = parse.getString(data.timezone);
 		}
 
-		if (data.date_format !== undefined) {
+		if (data.date_format) {
 			settings.date_format = parse.getString(data.date_format);
 		}
 
-		if (data.time_format !== undefined) {
+		if (data.time_format) {
 			settings.time_format = parse.getString(data.time_format);
 		}
 
-		if (data.default_shipping_country !== undefined) {
-			settings.default_shipping_country = parse.getString(
-				data.default_shipping_country
-			);
+		if (data.default_shipping_country) {
+			settings.default_shipping_country = parse.getString(data.default_shipping_country);
 		}
 
-		if (data.default_shipping_state !== undefined) {
-			settings.default_shipping_state = parse.getString(
-				data.default_shipping_state
-			);
+		if (data.default_shipping_state) {
+			settings.default_shipping_state = parse.getString(data.default_shipping_state);
 		}
 
-		if (data.default_shipping_city !== undefined) {
-			settings.default_shipping_city = parse.getString(
-				data.default_shipping_city
-			);
+		if (data.default_shipping_city) {
+			settings.default_shipping_city = parse.getString(data.default_shipping_city);
 		}
 
-		if (data.default_product_sorting !== undefined) {
-			settings.default_product_sorting = parse.getString(
-				data.default_product_sorting
-			);
+		if (data.default_product_sorting) {
+			settings.default_product_sorting = parse.getString(data.default_product_sorting);
 		}
 
-		if (data.product_fields !== undefined) {
+		if (data.product_fields) {
 			settings.product_fields = parse.getString(data.product_fields);
 		}
 
@@ -162,29 +153,24 @@ class SettingsService {
 			settings.products_limit = parse.getNumberIfPositive(data.products_limit);
 		}
 
-		if (data.weight_unit !== undefined) {
+		if (data.weight_unit) {
 			settings.weight_unit = parse.getString(data.weight_unit);
 		}
 
-		if (data.length_unit !== undefined) {
+		if (data.length_unit) {
 			settings.length_unit = parse.getString(data.length_unit);
 		}
 
-		if (data.logo_file !== undefined) {
+		if (data.logo_file) {
 			settings.logo_file = parse.getString(data.logo_file);
 		}
 
 		if (data.hide_billing_address !== undefined) {
-			settings.hide_billing_address = parse.getBooleanIfValid(
-				data.hide_billing_address,
-				false
-			);
+			settings.hide_billing_address = parse.getBooleanIfValid(data.hide_billing_address, false);
 		}
 
-		if (data.order_confirmation_copy_to !== undefined) {
-			settings.order_confirmation_copy_to = parse.getString(
-				data.order_confirmation_copy_to
-			);
+		if (data.order_confirmation_copy_to) {
+			settings.order_confirmation_copy_to = parse.getString(data.order_confirmation_copy_to);
 		}
 
 		return settings;
