4b0e2f9e7d88cc361dd3c633313b568956328e13
Jorge Feijoo <hola@jorgefeijoo.es>
2019-03-17 11:16:25 +0000

    settings fixes

--- src/services/settings/settings.js
+++ src/services/settings/settings.js
@@ -9,31 +9,33 @@ import { db } from '../../lib/mongo';
 import parse from '../../lib/parse';
 
 class SettingsService {
-	defaultSettings = {
-		store_name: 'Cezerin',
-		domain: '',
-		logo_file: 'assets/images/logo.png',
-		language: 'en',
-		currency_code: 'USD',
-		currency_symbol: '$',
-		currency_format: '${amount}',
-		thousand_separator: ',',
-		decimal_separator: '.',
-		decimal_number: 2,
-		timezone: 'Asia/Singapore',
-		date_format: 'MMMM D, YYYY',
-		time_format: 'h:mm a',
-		default_shipping_country: '',
-		default_shipping_state: '',
-		default_shipping_city: '',
-		default_product_sorting: 'stock_status,price,position',
-		product_fields:
-			'path,id,name,category_id,category_name,sku,images,enabled,discontinued,stock_status,stock_quantity,price,on_sale,regular_price,attributes,tags,position',
-		products_limit: 30,
-		weight_unit: 'kg',
-		length_unit: 'cm',
-		hide_billing_address: false,
-		order_confirmation_copy_to: ''
+	constructor() {
+		this.defaultSettings = {
+			store_name: '',
+			domain: '',
+			logo_file: null,
+			language: 'en',
+			currency_code: 'USD',
+			currency_symbol: '$',
+			currency_format: '${amount}',
+			thousand_separator: ',',
+			decimal_separator: '.',
+			decimal_number: 2,
+			timezone: 'Asia/Singapore',
+			date_format: 'MMMM D, YYYY',
+			time_format: 'h:mm a',
+			default_shipping_country: '',
+			default_shipping_state: '',
+			default_shipping_city: '',
+			default_product_sorting: 'stock_status,price,position',
+			product_fields:
+				'path,id,name,category_id,category_name,sku,images,enabled,discontinued,stock_status,stock_quantity,price,on_sale,regular_price,attributes,tags,position',
+			products_limit: 30,
+			weight_unit: 'kg',
+			length_unit: 'cm',
+			hide_billing_address: false,
+			order_confirmation_copy_to: ''
+		};
 	}
 
 	getSettings() {
@@ -81,71 +83,80 @@ class SettingsService {
 
 		let settings = {};
 
-		if (data.store_name) {
+		if (data.store_name !== undefined) {
 			settings.store_name = parse.getString(data.store_name);
 		}
 
-		if (data.language) {
+		if (data.language !== undefined) {
 			settings.language = parse.getString(data.language);
 		}
 
-		if (data.currency_code) {
+		if (data.currency_code !== undefined) {
 			settings.currency_code = parse.getString(data.currency_code);
 		}
 
-		if (data.domain) {
+		if (data.domain !== undefined) {
 			settings.domain = parse.getString(data.domain);
 		}
 
-		if (data.currency_symbol) {
+		if (data.currency_symbol !== undefined) {
 			settings.currency_symbol = parse.getString(data.currency_symbol);
 		}
 
-		if (data.currency_format) {
+		if (data.currency_format !== undefined) {
 			settings.currency_format = parse.getString(data.currency_format);
 		}
 
-		if (data.thousand_separator) {
+		if (data.thousand_separator !== undefined) {
 			settings.thousand_separator = parse.getString(data.thousand_separator);
 		}
 
-		if (data.decimal_separator) {
+		if (data.decimal_separator !== undefined) {
 			settings.decimal_separator = parse.getString(data.decimal_separator);
 		}
 
 		if (data.decimal_number !== undefined) {
-			settings.decimal_number = parse.getNumberIfPositive(data.decimal_number) || 0;
+			settings.decimal_number =
+				parse.getNumberIfPositive(data.decimal_number) || 0;
 		}
 
-		if (data.timezone) {
+		if (data.timezone !== undefined) {
 			settings.timezone = parse.getString(data.timezone);
 		}
 
-		if (data.date_format) {
+		if (data.date_format !== undefined) {
 			settings.date_format = parse.getString(data.date_format);
 		}
 
-		if (data.time_format) {
+		if (data.time_format !== undefined) {
 			settings.time_format = parse.getString(data.time_format);
 		}
 
-		if (data.default_shipping_country) {
-			settings.default_shipping_country = parse.getString(data.default_shipping_country);
+		if (data.default_shipping_country !== undefined) {
+			settings.default_shipping_country = parse.getString(
+				data.default_shipping_country
+			);
 		}
 
-		if (data.default_shipping_state) {
-			settings.default_shipping_state = parse.getString(data.default_shipping_state);
+		if (data.default_shipping_state !== undefined) {
+			settings.default_shipping_state = parse.getString(
+				data.default_shipping_state
+			);
 		}
 
-		if (data.default_shipping_city) {
-			settings.default_shipping_city = parse.getString(data.default_shipping_city);
+		if (data.default_shipping_city !== undefined) {
+			settings.default_shipping_city = parse.getString(
+				data.default_shipping_city
+			);
 		}
 
-		if (data.default_product_sorting) {
-			settings.default_product_sorting = parse.getString(data.default_product_sorting);
+		if (data.default_product_sorting !== undefined) {
+			settings.default_product_sorting = parse.getString(
+				data.default_product_sorting
+			);
 		}
 
-		if (data.product_fields) {
+		if (data.product_fields !== undefined) {
 			settings.product_fields = parse.getString(data.product_fields);
 		}
 
@@ -153,24 +164,29 @@ class SettingsService {
 			settings.products_limit = parse.getNumberIfPositive(data.products_limit);
 		}
 
-		if (data.weight_unit) {
+		if (data.weight_unit !== undefined) {
 			settings.weight_unit = parse.getString(data.weight_unit);
 		}
 
-		if (data.length_unit) {
+		if (data.length_unit !== undefined) {
 			settings.length_unit = parse.getString(data.length_unit);
 		}
 
-		if (data.logo_file) {
+		if (data.logo_file !== undefined) {
 			settings.logo_file = parse.getString(data.logo_file);
 		}
 
 		if (data.hide_billing_address !== undefined) {
-			settings.hide_billing_address = parse.getBooleanIfValid(data.hide_billing_address, false);
+			settings.hide_billing_address = parse.getBooleanIfValid(
+				data.hide_billing_address,
+				false
+			);
 		}
 
-		if (data.order_confirmation_copy_to) {
-			settings.order_confirmation_copy_to = parse.getString(data.order_confirmation_copy_to);
+		if (data.order_confirmation_copy_to !== undefined) {
+			settings.order_confirmation_copy_to = parse.getString(
+				data.order_confirmation_copy_to
+			);
 		}
 
 		return settings;
