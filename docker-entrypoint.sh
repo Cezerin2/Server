efd33d8a7972e680d4e88a0020e38d724efd4434
vam <vamshop@gmail.com>
2019-03-20 00:15:58 +0300

    Initial data setup added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 envsubst '${API_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 nginx
+exec npm run setup admin@example.com http://localhost:3000
 exec pm2 start process.json --no-daemon
