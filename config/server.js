82f228edd2263a2759d06c8c625b62533eda82d9
sevgit <sebastian.ventura@q.media>
2019-09-18 20:40:41 -0300

    Added env config

--- config/server.js
+++ config/server.js
@@ -1,14 +1,6 @@
 // config used by server side only
-const dbHost = process.env.DB_HOST || '127.0.0.1';
-const dbPort = process.env.DB_PORT || 27017;
-const dbName = process.env.DB_NAME || 'shop';
-const dbUser = process.env.DB_USER || '';
-const dbPass = process.env.DB_PASS || '';
-const dbCred =
-	dbUser.length > 0 || dbPass.length > 0 ? `${dbUser}:${dbPass}@` : '';
-
-const dbUrl =
-	process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`;
+require('dotenv/config');
+const dbUrl = process.env.DB_URL;
 
 module.exports = {
 	// used by Store (server side)
