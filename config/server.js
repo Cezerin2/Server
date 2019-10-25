0674738687e438242ab869adebd3f97c4371f163
VamShop <vamshop@gmail.com>
2019-10-25 19:47:26 +0300

    Docs fix

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 // config used by server side only
 const dbHost = process.env.DB_HOST || '127.0.0.1';
 const dbPort = process.env.DB_PORT || 27017;
-const dbName = process.env.DB_NAME || 'shop';
+const dbName = process.env.DB_NAME || 'cezerin';
 const dbUser = process.env.DB_USER || '';
 const dbPass = process.env.DB_PASS || '';
 const dbCred =
