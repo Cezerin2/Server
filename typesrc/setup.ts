058d096ee48f0178c242be653a33cc0619a70d37
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-09 06:52:09 +0700

    Update setup.ts

--- typesrc/setup.ts
+++ typesrc/setup.ts
@@ -1,7 +1,6 @@
 import winston from 'winston';
 import url from 'url';
 import { MongoClient } from 'mongodb';
-import logger from './lib/logger';
 import settings from './lib/settings';
 
 const mongodbConnection = settings.mongodbServerUrl;
@@ -9,10 +8,8 @@ const mongoPathName = url.parse(mongodbConnection).pathname;
 const dbName = mongoPathName.substring(mongoPathName.lastIndexOf('/') + 1);
 
 const CONNECT_OPTIONS = {
-	useNewUrlParser: true
+	useNewUrlParser: true,
 };
-
-const DEFAULT_LANGUAGE = 'english';
 
 const addPage = async (db, pageObject) => {
 	const count = await db
@@ -25,64 +22,64 @@ const addPage = async (db, pageObject) => {
 	}
 };
 
-const addAllPages = async db => {
+const addAllPages = async (db) => {
 	await addPage(db, {
 		slug: '',
 		meta_title: 'Home',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'checkout',
 		meta_title: 'Checkout',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'checkout-success',
 		meta_title: 'Thank You!',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'about',
 		meta_title: 'About us',
 		enabled: true,
-		is_system: false
+		is_system: false,
 	});
 	await addPage(db, {
 		slug: 'login',
 		meta_title: 'Login',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'register',
 		meta_title: 'Register',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'customer-account',
 		meta_title: 'Customer Account',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'forgot-password',
 		meta_title: 'Forgot Password',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 	await addPage(db, {
 		slug: 'reset-password',
 		meta_title: 'Reset Password',
 		enabled: true,
-		is_system: true
+		is_system: true,
 	});
 };
 
-const addAllProducts = async db => {
+const addAllProducts = async (db) => {
 	const productCategoriesCount = await db
 		.collection('productCategories')
 		.countDocuments({});
@@ -97,7 +94,7 @@ const addAllProducts = async db => {
 			slug: 'category-a',
 			image: '',
 			parent_id: null,
-			enabled: true
+			enabled: true,
 		});
 
 		const catB = await db.collection('productCategories').insertOne({
@@ -105,7 +102,7 @@ const addAllProducts = async db => {
 			slug: 'category-b',
 			image: '',
 			parent_id: null,
-			enabled: true
+			enabled: true,
 		});
 
 		const catC = await db.collection('productCategories').insertOne({
@@ -113,7 +110,7 @@ const addAllProducts = async db => {
 			slug: 'category-c',
 			image: '',
 			parent_id: null,
-			enabled: true
+			enabled: true,
 		});
 
 		const catA1 = await db.collection('productCategories').insertOne({
@@ -121,7 +118,7 @@ const addAllProducts = async db => {
 			slug: 'category-a-1',
 			image: '',
 			parent_id: catA.insertedId,
-			enabled: true
+			enabled: true,
 		});
 
 		const catA2 = await db.collection('productCategories').insertOne({
@@ -129,7 +126,7 @@ const addAllProducts = async db => {
 			slug: 'category-a-2',
 			image: '',
 			parent_id: catA.insertedId,
-			enabled: true
+			enabled: true,
 		});
 
 		const catA3 = await db.collection('productCategories').insertOne({
@@ -137,7 +134,7 @@ const addAllProducts = async db => {
 			slug: 'category-a-3',
 			image: '',
 			parent_id: catA.insertedId,
-			enabled: true
+			enabled: true,
 		});
 
 		await db.collection('products').insertOne({
@@ -150,8 +147,8 @@ const addAllProducts = async db => {
 			discontinued: false,
 			attributes: [
 				{ name: 'Brand', value: 'Brand A' },
-				{ name: 'Size', value: 'M' }
-			]
+				{ name: 'Size', value: 'M' },
+			],
 		});
 
 		await db.collection('products').insertOne({
@@ -164,15 +161,15 @@ const addAllProducts = async db => {
 			discontinued: false,
 			attributes: [
 				{ name: 'Brand', value: 'Brand B' },
-				{ name: 'Size', value: 'L' }
-			]
+				{ name: 'Size', value: 'L' },
+			],
 		});
 
 		winston.info('- Added products');
 	}
 };
 
-const addOrderConfirmationEmailTemplates = async db => {
+const addOrderConfirmationEmailTemplates = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'order_confirmation' });
@@ -231,14 +228,14 @@ const addOrderConfirmationEmailTemplates = async db => {
 			  </tr>
 			</table>
 		  
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Order Confirmation');
 	}
 };
 
-const addForgotPasswordEmailTemplates_en = async db => {
+const addForgotPasswordEmailTemplates_en = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'forgot_password_en' });
@@ -253,14 +250,14 @@ const addForgotPasswordEmailTemplates_en = async db => {
 			<div><b>Please click on the link bellow to get back to {{shop_name}} to reset your password.</b><div>
 			<div><b>Link:</b> {{forgot_password_link}}<div>
 
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Password Reset English');
 	}
 };
 
-const addForgotPasswordEmailTemplates_ru = async db => {
+const addForgotPasswordEmailTemplates_ru = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'forgot_password_ru' });
@@ -275,14 +272,14 @@ const addForgotPasswordEmailTemplates_ru = async db => {
 			<div><b>Пожалуйста, перейдите по указанной ссылке для сброса пароля.</b><div>
 			<div><b>Ссылка:</b> {{forgot_password_link}}<div>
 
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Password Reset Russian');
 	}
 };
 
-const addForgotPasswordEmailTemplates_de = async db => {
+const addForgotPasswordEmailTemplates_de = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'forgot_password_de' });
@@ -302,14 +299,14 @@ const addForgotPasswordEmailTemplates_de = async db => {
 			<div><b>Beste Grüße</b></div>
 			<br />
 			<div><b><h3>{{shop_name}}</h3></b><div>
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Password Reset German');
 	}
 };
 
-const addRegisterDoiEmailTemplates_en = async db => {
+const addRegisterDoiEmailTemplates_en = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'register_doi_en' });
@@ -331,14 +328,14 @@ const addRegisterDoiEmailTemplates_en = async db => {
 			<br />
 			<div><b><h3>{{shop_name}}</h3></b><div>
 
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Account Activation English');
 	}
 };
 
-const addRegisterDoiEmailTemplates_ru = async db => {
+const addRegisterDoiEmailTemplates_ru = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'register_doi_ru' });
@@ -356,14 +353,14 @@ const addRegisterDoiEmailTemplates_ru = async db => {
 			<br />
 			<div><b>Если у Вас есть какие-либо вопросы, задайте нам их в ответном письме.</b></div>
 
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Account Activation Russian');
 	}
 };
 
-const addRegisterDoiEmailTemplates_de = async db => {
+const addRegisterDoiEmailTemplates_de = async (db) => {
 	const emailTemplatesCount = await db
 		.collection('emailTemplates')
 		.countDocuments({ name: 'register_doi_de' });
@@ -384,14 +381,14 @@ const addRegisterDoiEmailTemplates_de = async db => {
 			<div><b>Beste Grüße</b></div>
 			<br />
 			<div><b><h3>{{shop_name}}</h3></b><div>
-		  </div>`
+		  </div>`,
 		});
 
 		winston.info('- Added email template for Account Activation German');
 	}
 };
 
-const addShippingMethods = async db => {
+const addShippingMethods = async (db) => {
 	const shippingMethodsCount = await db
 		.collection('shippingMethods')
 		.countDocuments({});
@@ -407,14 +404,14 @@ const addShippingMethods = async db => {
 				subtotal_min: 0,
 				subtotal_max: 0,
 				weight_total_min: 0,
-				weight_total_max: 0
-			}
+				weight_total_max: 0,
+			},
 		});
 		winston.info('- Added shipping method');
 	}
 };
 
-const addPaymentMethods = async db => {
+const addPaymentMethods = async (db) => {
 	const paymentMethodsCount = await db
 		.collection('paymentMethods')
 		.countDocuments({});
@@ -427,21 +424,18 @@ const addPaymentMethods = async db => {
 				countries: [],
 				shipping_method_ids: [],
 				subtotal_min: 0,
-				subtotal_max: 0
-			}
+				subtotal_max: 0,
+			},
 		});
 		winston.info('- Added payment method');
 	}
 };
 
-const createIndex = (db, collectionName, fields, options) =>
-	db.collection(collectionName).createIndex(fields, options);
+const createIndex = (db, collectionName, fields) =>
+	db.collection(collectionName).createIndex(fields);
 
-const createAllIndexes = async db => {
-	const pagesIndexes = await db
-		.collection('pages')
-		.listIndexes()
-		.toArray();
+const createAllIndexes = async (db) => {
+	const pagesIndexes = await db.collection('pages').listIndexes().toArray();
 
 	if (pagesIndexes.length === 1) {
 		await createIndex(db, 'pages', { enabled: 1 });
@@ -472,17 +466,12 @@ const createAllIndexes = async db => {
 		await createIndex(db, 'products', { sku: 1 });
 		await createIndex(db, 'products', {
 			'attributes.name': 1,
-			'attributes.value': 1
+			'attributes.value': 1,
 		});
-		await createIndex(
-			db,
-			'products',
-			{
-				name: 'text',
-				description: 'text'
-			},
-			{ default_language: DEFAULT_LANGUAGE, name: 'textIndex' }
-		);
+		await createIndex(db, 'products', {
+			name: 'text',
+			description: 'text',
+		});
 		winston.info('- Created indexes for: products');
 	}
 
@@ -493,27 +482,19 @@ const createAllIndexes = async db => {
 
 	if (customersIndexes.length === 1) {
 		await createIndex(db, 'customers', { group_id: 1 });
-		await createIndex(db, 'customers', { email: 1 }, { unique: true });
+		await createIndex(db, 'customers', { email: 1 });
 		await createIndex(db, 'customers', { mobile: 1 });
 		await createIndex(db, 'customers', { first_name: 1 });
 		await createIndex(db, 'customers', { last_name: 1 });
 		await createIndex(db, 'customers', { password: 1 });
-		await createIndex(
-			db,
-			'customers',
-			{
-				full_name: 'text',
-				'addresses.address1': 'text'
-			},
-			{ default_language: DEFAULT_LANGUAGE, name: 'textIndex' }
-		);
+		await createIndex(db, 'customers', {
+			full_name: 'text',
+			'addresses.address1': 'text',
+		});
 		winston.info('- Created indexes for: customers');
 	}
 
-	const ordersIndexes = await db
-		.collection('orders')
-		.listIndexes()
-		.toArray();
+	const ordersIndexes = await db.collection('orders').listIndexes().toArray();
 
 	if (ordersIndexes.length === 1) {
 		await createIndex(db, 'orders', { draft: 1 });
@@ -524,15 +505,10 @@ const createAllIndexes = async db => {
 		await createIndex(db, 'orders', { first_name: 1 });
 		await createIndex(db, 'orders', { last_name: 1 });
 		await createIndex(db, 'orders', { password: 1 });
-		await createIndex(
-			db,
-			'orders',
-			{
-				'shipping_address.full_name': 'text',
-				'shipping_address.address1': 'text'
-			},
-			{ default_language: DEFAULT_LANGUAGE, name: 'textIndex' }
-		);
+		await createIndex(db, 'orders', {
+			'shipping_address.full_name': 'text',
+			'shipping_address.address1': 'text',
+		});
 		winston.info('- Created indexes for: orders');
 	}
 };
@@ -540,7 +516,7 @@ const createAllIndexes = async db => {
 const addUser = async (db, userEmail) => {
 	if (userEmail && userEmail.includes('@')) {
 		const tokensCount = await db.collection('tokens').countDocuments({
-			email: userEmail
+			email: userEmail,
 		});
 		const tokensNotExists = tokensCount === 0;
 
@@ -551,7 +527,7 @@ const addUser = async (db, userEmail) => {
 				expiration: 72,
 				name: 'Owner',
 				email: userEmail,
-				scopes: ['admin']
+				scopes: ['admin'],
 			});
 			winston.info(`- Added token with email: ${userEmail}`);
 		}
@@ -564,8 +540,8 @@ const addSettings = async (db, { domain }) => {
 			{},
 			{
 				$set: {
-					domain
-				}
+					domain,
+				},
 			},
 			{ upsert: true }
 		);
@@ -605,7 +581,7 @@ const addSettings = async (db, { domain }) => {
 	await createAllIndexes(db);
 	await addUser(db, userEmail);
 	await addSettings(db, {
-		domain
+		domain,
 	});
 
 	client.close();
