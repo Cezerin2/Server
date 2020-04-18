d6ca95f36c1ff097e866f122ad71bc70aa008fda
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-18 19:49:59 +0700

    Lint

--- src/routes/paymentGateways.ts
+++ src/routes/paymentGateways.ts
@@ -1,36 +1,42 @@
-import security from '../lib/security';
-import PaymentGatewaysService from '../services/settings/paymentGateways';
+import security from '../lib/security'
+import PaymentGatewaysService from '../services/settings/paymentGateways'
 
 class PaymentGatewaysRoute {
-	constructor(router) {
-		this.router = router;
-		this.registerRoutes();
-	}
-	router;
-	registerRoutes() {
-		this.router.get(
-			'/v1/payment_gateways/:name',
-			security.checkUserScope.bind(this, security.scope.READ_PAYMENT_METHODS),
-			this.getGateway.bind(this)
-		);
-		this.router.put(
-			'/v1/payment_gateways/:name',
-			security.checkUserScope.bind(this, security.scope.WRITE_PAYMENT_METHODS),
-			this.updateGateway.bind(this)
-		);
-	}
+    constructor(router) {
+        this.router = router
+        this.registerRoutes()
+    }
+    router
+    registerRoutes() {
+        this.router.get(
+            '/v1/payment_gateways/:name',
+            security.checkUserScope.bind(
+                this,
+                security.scope.READ_PAYMENT_METHODS
+            ),
+            this.getGateway.bind(this)
+        )
+        this.router.put(
+            '/v1/payment_gateways/:name',
+            security.checkUserScope.bind(
+                this,
+                security.scope.WRITE_PAYMENT_METHODS
+            ),
+            this.updateGateway.bind(this)
+        )
+    }
 
-	getGateway(req, res, next) {
-		PaymentGatewaysService.getGateway(req.params.name)
-			.then((data) => res.send(data))
-			.catch(next);
-	}
+    getGateway(req, res, next) {
+        PaymentGatewaysService.getGateway(req.params.name)
+            .then((data) => res.send(data))
+            .catch(next)
+    }
 
-	updateGateway(req, res, next) {
-		PaymentGatewaysService.updateGateway(req.params.name, req.body)
-			.then((data) => res.send(data))
-			.catch(next);
-	}
+    updateGateway(req, res, next) {
+        PaymentGatewaysService.updateGateway(req.params.name, req.body)
+            .then((data) => res.send(data))
+            .catch(next)
+    }
 }
 
-export default PaymentGatewaysRoute;
+export default PaymentGatewaysRoute
--- src/services/theme/assets.ts
+++ src/services/theme/assets.ts
@@ -1,20 +1,20 @@
-import AssetService from '../assets/assets';
-import settings from '../../lib/settings';
+import AssetService from '../assets/assets'
+import settings from '../../lib/settings'
 
-const ThemeAssetPath = `${settings.assetServer.themeImageUploadPath}`;
+const ThemeAssetPath = `${settings.assetServer.themeImageUploadPath}`
 
 class ThemeAssetsService {
-	deleteFile(fileName) {
-		return AssetService.deleteFile(ThemeAssetPath, fileName);
-	}
+    deleteFile(fileName) {
+        return AssetService.deleteFile(ThemeAssetPath, fileName)
+    }
 
-	uploadFile(req, res) {
-		AssetService.uploadFile(req, res, ThemeAssetPath, () => {});
-	}
+    uploadFile(req, res) {
+        AssetService.uploadFile(req, res, ThemeAssetPath, () => {})
+    }
 
-	getErrorMessage(err) {
-		return { error: true, message: err.toString() };
-	}
+    getErrorMessage(err) {
+        return { error: true, message: err.toString() }
+    }
 }
 
-export default new ThemeAssetsService();
+export default new ThemeAssetsService()
