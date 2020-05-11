c04fb34ffabd873f5d9a22f7de48982b0b532c0f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-11 16:58:16 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,15 @@
+const enviroment = process.env
 // config used by server side only
-const dbHost = process.env.DB_HOST || "127.0.0.1"
-const dbPort = process.env.DB_PORT || 27017
-const dbName = process.env.DB_NAME || "shop"
-const dbUser = process.env.DB_USER || ""
-const dbPass = process.env.DB_PASS || ""
+const dbHost = enviroment.DB_HOST || "127.0.0.1"
+const dbPort = enviroment.DB_PORT || 27017
+const dbName = enviroment.DB_NAME || "shop"
+const dbUser = enviroment.DB_USER || ""
+const dbPass = enviroment.DB_PASS || ""
 const dbCred =
   dbUser.length > 0 || dbPass.length > 0 ? `${dbUser}:${dbPass}@` : ""
 
 const dbUrl =
-  process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`
-
-const enviroment = process.env
+  enviroment.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`
 
 const Config = {
   // used by Store (server side)
