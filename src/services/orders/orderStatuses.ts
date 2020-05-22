ba756ca0458e04ea658862d956154da6a2fa8109
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-23 03:51:22 +0700

    Update orderStatuses.ts

--- src/services/orders/orderStatuses.ts
+++ src/services/orders/orderStatuses.ts
@@ -3,8 +3,8 @@ import { db } from "../../lib/mongo"
 import parse from "../../lib/parse"
 
 class OrderStatusesService {
-  getStatuses(params = {}) {
-    const filter = {}
+  getStatuses(params = { id: {} }) {
+    const filter = { _id: {} }
     const id = parse.getObjectIDIfValid(params.id)
     if (id) {
       filter._id = new ObjectID(id)
